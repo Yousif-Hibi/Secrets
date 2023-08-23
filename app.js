8ad0aa147d3d53f7aa9aa79d57efcf0a17ba66f7
Yousif-Hibi <yousif.hibi@gmail.com>
2023-08-23 09:48:21 +0300

    fix

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const express  = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const  encrypt = require("mongoose-encryption");
-
+//1.const  encrypt = require("mongoose-encryption");
+//2.const md5 = require('md5');
+const bcrypt =require("bcrypt");
+const saltRounds = 10;
 
 
 const app = express();
@@ -26,7 +28,7 @@ password: String
 
 
 
-    userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields:["password"] });
+   //1. userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields:["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -43,27 +45,40 @@ app.get("/register", function (req, res) {
 });
 
 app.post("/register", function (req, res) {
-    const newUser= new User({
-        email: req.body.username,
-        password: req.body.password
-    });
-    newUser.save()
-    .then(function(){   
-        res.render("secrets");
-    }).catch(function (err) {
-        console.log(err);
-    });
+
+   bcrypt.hash(req.body.password,saltRounds,function(err,hash){
+       const newUser = new User({
+           email: req.body.username,
+           //2. password: md5(req.body.password)
+           password: hash
+       });
+       newUser.save()
+           .then(function () {
+               res.render("secrets");
+           }).catch(function (err) {
+               console.log(err);
+           });
+
+   })
+
+    
 });
 
 app.post("/login",function(req,res){
     const username = req.body.username;
-    const password= req.body.password;
+    //2. const password= md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({email: username}).then(function(foundUser){
-        if(foundUser.password === password){
-           
+
+        bcrypt.compare(password,foundUser.password,function(err,result){
+            if (result===true)
             res.render("secrets");
-        }
+
+        })
+       
+           
+            
     }).catch(function(err){
         console.log(err);
     });
